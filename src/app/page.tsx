82d86c650346eb9bbac0389c65e00bc8218a8896
SaurabPoudel <poudelsaurab20@gmail.com>
2023-11-15 22:56:34 +0545

    in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <div className="text-red-800 text-3xl">
+        WORK IN PROGRESS .. CHECK THIS AFTER 5 DAYS
+      </div>
     </main>
   );
 }
