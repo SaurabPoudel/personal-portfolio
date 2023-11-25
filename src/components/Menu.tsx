842b7d437f42c4fc85e4a80ee081a52239b6619a
SaurabPoudel <poudelsaurab20@gmail.com>
2023-11-25 09:19:16 +0545

    greater than symbol

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,7 +4,7 @@ const Menu = () => {
   return (
     <section className="flex justify-center">
       <div className="pt-10 px-8 md:max-w-6xl w-full text-lime-500">
-        Checkout my blog ->
+        Checkout my blog -&gt;
         <Link href="https://saurabblog.vercel.app" target="_blank" className="px-5">saurabblog</Link>
       </div>
     </section>
